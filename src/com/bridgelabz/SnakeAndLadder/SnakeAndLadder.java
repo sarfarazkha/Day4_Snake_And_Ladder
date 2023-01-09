e6d3f8b744c5720a0e3b13757c410d7ee85951b1
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-09 22:27:55 +0530

    Added: Start with position zero

---
+++ src/com/bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("the initial position of player one:" + position);
+    }
+}
